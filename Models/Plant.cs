b8a857f8bc6f55e7702d65372fee312c87d223ac
Benjamin Thom and Fatma Dogan <benjamin+fatma@>
2020-02-27 15:37:24 -0800

    update Program.cs and Plant.cs to include logic for Plant has died

--- Models/Plant.cs
+++ Models/Plant.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Models
 {
   public class Plant
@@ -6,12 +8,14 @@ namespace Models
     public int PlantHealth {get; set;}
 
     public string StateOfMind {get; set;}
+    public string PlantTroubles {get; set;}
 
   public Plant(string name)
   {
     Name = name;
     PlantHealth = 5;
     StateOfMind = "neutral";
+    PlantTroubles = " ";
   }
 
   public void DetermineNextSteps(string addPlantHealth)
@@ -19,20 +23,22 @@ namespace Models
     if (addPlantHealth == "water")
     {
       AddWater();
+      RemoveHealth();
       UpdateMood();
-      IsPlantDead();
     }
     else if (addPlantHealth == "food")
     {
       AddFood();
+      RemoveHealth();
       UpdateMood();
-      IsPlantDead();
+      
     }
     else if (addPlantHealth == "sun")
     {
       AddSunshine();
+      RemoveHealth();
       UpdateMood();
-      IsPlantDead();
+      
     }
   }
 
@@ -42,27 +48,16 @@ namespace Models
     {
       StateOfMind = "healthy";
     }
-    else if( PlantHealth < 10)
+    else if( PlantHealth < 10 && PlantHealth >= 1)
     {
       StateOfMind = "unhealthy";
     }
-    else if ( PlantHealth == 0)
+    else if ( PlantHealth < 1)
     {
       StateOfMind = "dead";
     }
   }
 
-  private string IsPlantDead()
-  {
-    if (StateOfMind == "dead")
-    {
-      return "Your plant died.";
-    }
-    else 
-    {
-      return "Keep playing...";
-    }
-  }
   private void AddWater()
   {
     PlantHealth += 1;
@@ -76,6 +71,29 @@ namespace Models
   private void AddSunshine()
   {
     PlantHealth += 3;
+  }
+
+  private void RemoveHealth()
+  {
+    var rand = new Random();
+    int index = rand.Next(4);
+
+    if (index == 1)
+    {
+      PlantHealth -= 1;
+      PlantTroubles = "Oh no, a windstrom damaged your plant!";
+    }
+    else if (index == 2)
+    {
+      PlantHealth -= 2;
+      PlantTroubles = "Yikes, there was an aphid attack!";
+    }
+    else if (index == 3)
+    {
+      PlantHealth -= 3;
+      PlantTroubles = "Your plant was bit by a slug!";
+    }
+   
   }
 
   }
--- Program.cs
+++ Program.cs
@@ -5,23 +5,35 @@ namespace GrowYourOwnPlant
 {
   public class Program
   {
+    static string plantName;
+    static Plant plantOne = new Plant(plantName);
     public static void Main()
     {
       Console.WriteLine("It's time to grow your own plant! What would you like to name your plant?");
-      string plantName = Console.ReadLine();
-
-      Plant plantOne = new Plant(plantName);
+      plantName = Console.ReadLine();
       Game();
 
-    void Game()
+    }
+     
+    public static void Game()
     {  
       Console.WriteLine("Keep " + plantName + " alive, to water your plant, type 'water', to feed your plant, type 'food' and to provide sunshine, type'sun'");
       string addPlantHealth = Console.ReadLine().ToLower();
       Console.WriteLine("-----------------------------");
       plantOne.DetermineNextSteps(addPlantHealth);
       Console.WriteLine(plantName + " is " + plantOne.StateOfMind);
-      Game();
-    }
+      Console.WriteLine(plantOne.PlantTroubles);
+      Console.WriteLine(plantName +"'s " + "Health Level: " + plantOne.PlantHealth);
+      Console.WriteLine("------------");
+      if (plantOne.StateOfMind == "dead")
+      {
+        Console.WriteLine("Your plant has died");
+      }
+      else
+      {
+        Console.WriteLine("Keep playing");
+        Game();
+      }
     }
   }
 }
